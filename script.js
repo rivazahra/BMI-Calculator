02c442052130736078c9f25e8de06d2f401996c8
rivazahra <riva.zahra88@gmail.com>
2023-09-27 14:24:19 +0700

    Add logic completely

--- script.js
+++ script.js
@@ -1,43 +1,61 @@
-function getData(){
+function getData() {
+  let weight = document.getElementById("berat").value;
+  let height = document.getElementById("tinggi").value;
 
-    let weight = document.getElementById('berat').value;
-    let height = document.getElementById('tinggi').value;
-
-    return{
-        weight,height
-    }
+  return {
+    weight,
+    height,
+  };
 }
 
 function isNumber(string) {
-    if (isNaN(string)) {
-      return false;
-    }
-    return true;
+  if (isNaN(string)) {
+    return false;
   }
-  
-  let validasiData = getData()
-  function validasiInput(validasiData){
-    let weight = document.getElementById('berat').value
-    let height = document.getElementById('tinggi').value
-    if(validasiData !==null && isNumber(validasiData.weight, validasiData.height)){
-        return true
-    }
+  return true;
+}
 
-    let hasil = document.getElementById('hasil')
-    let bmi = weight / ((height/100) **2)
-bmi = bmi.toFixed(1)
-    if(bmi <= 18.5){
-        hasil.textContent = ` Your BMI is ${bmi} which means You Are Underweight :(`;
-    }else if(bmi>=18.5 && bmi <= 24.9){
-        hasil.textContent = ` Your BMI is ${bmi} which means You Are Normal`;
+let validasiData = getData();
+let hasil = document.getElementById("hasil");
 
-    }else if( bmi >=25 && bmi<= 29.9){
-        hasil.textContent = ` Your BMI is ${bmi} which means You Are Overweight :(`;
+// function validasiInput(validasiData){
+//     if (validasiData !== null && isNumber(validasiData.weight, validasiData.height) ) {
+//       return true;
+//     }else{
+//         return false
+//     }
 
-    }else{
-        hasil.textContent = ` OMG BMI is ${bmi} which means You Are Obesity, get A DIET`;
+// }
+function submit() {
 
-    }
+  let weight = document.getElementById("berat").value;
+  let height = document.getElementById("tinggi").value;
+
+  if (!isNumber(weight) || !isNumber(height)) {
+    hasil.textContent = "Weight and height must be valid numbers.";
+    return;
   }
 
-  let submit = document.getElementById('check').addEventListener('click',validasiInput)
+  let bmi = weight / (height / 100) ** 2;
+  bmi = bmi.toFixed(1);
+  let category = "";
+
+   if (weight.trim() === "" || height.trim() === "") {
+    hasil.textContent = `Weight and height can't be empty`;
+    return;
+  } else if (bmi <= 18.5) {
+    category = `<span style="font-weight: bold;">Underweight 😣</span>`;
+  } else if (bmi >= 18.5 && bmi <= 24.9) {
+    category = '<span style="font-weight: bold;">Normal 😃 </span>';
+  } else if (bmi >= 25 && bmi <= 29.9) {
+    category = '<span style="font-weight: bold;">Overweight 😣</span>';
+  } else if (bmi >= 30) {
+    category = '<span style="font-weight: bold;">Obesity, GET ON DIET 😱 </span>';
+  } else if (weight == "" && height == "") {
+    hasil.textContent = "Periksa input sekali lagi";
+  }
+
+  hasil.innerHTML = ` Your BMI is ${bmi} which means You Are ${category}`;
+}
+
+let btnsubmit = document.getElementById("check").addEventListener("click", submit);
